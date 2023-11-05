c22d22fd3c69b12c69c0af5a48aa5e2e4f6983df
Roy Liao <liaolc@bc.edu>
2023-11-05 11:11:59 -0500

    fxed home, preparing to merge with Development

--- client/src/pages/Header/Header.jsx
+++ client/src/pages/Header/Header.jsx
@@ -1,7 +1,9 @@
 import "./Header.css";
 import logo from "./connect.png";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "../../components/LoginButton";
+import LogoutButton from "../../components/LogoutButton";
 
 const Header = () => {
   return (
@@ -12,31 +14,61 @@ const Header = () => {
 };
 
 function Nav_Header() {
+  const { isAuthenticated } = useAuth0();
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container-fluid">
-          <img src={logo} class="logo"/>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div className="container-fluid">
+          <img src={logo} className="logo" />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link to={"/"}> <button class="btn btn-outline-success" type="submit">Home</button> </Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to={"/"}>
+                  {" "}
+                  <button className="btn btn-outline-success" type="submit">
+                    Home
+                  </button>{" "}
+                </Link>
               </li>
-              <li class="nav-item">
-                <Link to={"/profile"}> <button class="btn btn-outline-success" type="submit">Profile</button> </Link>
+              <li className="nav-item">
+                <Link to={"/profile"}>
+                  {" "}
+                  <button className="btn btn-outline-success" type="submit">
+                    Profile
+                  </button>{" "}
+                </Link>
               </li>
             </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <Link to={"/search"}> <button class="btn btn-outline-success" type="submit">Search</button> </Link>
+            {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <Link to={"/search"}>
+                {" "}
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>{" "}
+              </Link>
             </form>
           </div>
         </div>
       </nav>
-    </> 
+    </>
   );
 }
 
