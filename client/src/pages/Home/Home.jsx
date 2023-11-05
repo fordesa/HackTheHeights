0f2126b7d6d3f6115ede97533f91f3125d8dc10e
Roy Liao <liaolc@bc.edu>
2023-11-05 03:16:55 -0500

    Home page finished front end

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -70,157 +70,231 @@ function About() {
         <div id="spacer">
       
       </div>
-      <div class="row connections my-4 rounded-3 justify-content-center">
-        <div class=" bg-primary col-7 me-3">
-          <h3>My Connections</h3>
+      <div class="row connections mt-4 pb-4 rounded-3 justify-content-center">
+        <div class=" bg-white card col-7 me-3 rounded-3">
+          <h3 class="m-2">My Connections</h3>
           <div class="row row-cols-2">
 
-            <div class="col">
-              <div className="container bg-secondary">
-                <div class="d-flex justify-content-start align-items-center">
-                  <img src="https://picsum.photos/100/100" class="rounded-circle p-2 me-2 " width="80" ></img>
+            <div class="col mb-3">
+              <div className="grow container bg-white rounded-3 card shadow">
+                <div class="d-flex flex-column text-center justify-content-center align-items-center">
+                  <img src="https://picsum.photos/150/150" class="rounded-circle border p-2 my-2 mt-3 " width="150" ></img>
                   <div class="text-container ">
-                    <p class="p-0 m-0"><b><span class="names">Thomas Jefferson</span></b><br />
-                    Boston College</p>
+                    <p class="p-0 mb-2"><b><span class="names ">Thomas Jefferson</span></b><br />
+                    Boston College &#8226; Computer Science<br/>Brownsburg, IN</p>
                   </div>
-                  <button type="button" class="btn btn-lg btn-primary ms-auto ">Chat</button>
-                </div>
-                <div class="content-container ">
-                  <p class="p-0 m-0">Computer Science &#8226; 2025 </p>
-                  <p class="p-0 m-0">From: Brownsburg, IN</p>
-                  <p class="p-0 m-0">Interests & Involvements</p>
-                  <ul>
-                    <li>Maker</li>
-                    <li>Full Stack Developer</li>
-                    <li>Robotics</li>
-                  </ul>
-                  <p>I'm open to: </p>
-                </div>
-                <div class="open-to-icons d-flex justify-content-evenly mx-4">
-                  <div class="d-flex col-3 justify-content-center">
-                    <img src={mentorIcon} width="45" height="auto"/>
+                  <div class="open-to-icons d-flex justify-content-evenly my-1">
+                    <div class="icons d-flex col p-2 mx-2 justify-content-center border border-primary rounded-circle">
+                      <img src={mentorIcon} class="" width="30" height="auto" title="Mentoring"/>
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={videoIcon} width="30" height="auto" />
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={essayIcon} width="30" height="auto"/>
+                    </div>
                   </div>
-                  <div class="d-flex col-3 justify-content-center">
-                    <img src={videoIcon} width="45" height="auto"/>
+                 
+                  <button type="button" class="btn btn-lg rounded-pill shadow-sm btn-primary my-3">Chat Now</button>
+                
+                  <div class="content-container ">
+                    <p class="pb-2 mb-2">Interests & Involvements</p>
+                    <div class="row d-flex justify-content-evenly align-items-center ">
+                      <div class="col "><p>Maker</p></div>
+                      <div class="col "><p>Full Stack Developer</p></div>
+                      <div class="col "><p>Robotics</p></div>
+                    </div>
                   </div>
-                  <div class="d-flex col-3 justify-content-center">
-                    <img src={essayIcon} width="45" height="auto"/>
-                  </div>
-                </div>
-                <div class="open-to-icons d-flex justify-content-evenly mx-4">
-                  <div class="d-flex col-3 justify-content-center">
-                    <p>Mentoring</p>
-                  </div>
-                  <div class="d-flex col-3 justify-content-center">
-                    <p>Video Call</p>
-                  </div>
-                  <div class="d-flex col-3 justify-content-center">
-                    <p>Essay Help</p>
-                  </div>
+                  
                 </div>
               </div>
             </div>
 
-            <div class="col ">
-              <div className="container  bg-secondary">
-                <div class="d-flex justify-content-start align-items-center">
-                  <img src="https://picsum.photos/100/100" class="rounded-circle p-2 me-2 " width="80" ></img>
+            <div class="col mb-3">
+              <div className="grow container bg-white rounded-3 card shadow">
+                <div class="d-flex flex-column text-center justify-content-center align-items-center">
+                  <img src="https://picsum.photos/150/150" class="rounded-circle border p-2 my-2 mt-3 " width="150" ></img>
                   <div class="text-container ">
-                    <p class="p-0 m-0"><b><span class="names">Thomas Jefferson</span></b><br />
-                    Boston College</p>
+                    <p class="p-0 mb-2"><b><span class="names ">Thomas Jefferson</span></b><br />
+                    Boston College &#8226; Computer Science<br/>Brownsburg, IN</p>
                   </div>
-                  <button type="button" class="btn btn-lg btn-primary ms-auto ">Chat</button>
-                </div>
-                <div class="content-container ">
-                  <p class="p-0 m-0">Computer Science &#8226; 2025 </p>
-                  <p class="p-0 m-0">From: Brownsburg, IN</p>
-                  <p class="p-0 m-0">Interests & Involvements</p>
-                  <ul>
-                    <li>Maker</li>
-                    <li>Full Stack Developer</li>
-                    <li>Robotics</li>
-                  </ul>
-                  <p>I'm open to: </p>
-                </div>
-                <div class="open-to-icons d-flex justify-content-evenly mx-4">
-                  <div class="d-flex col-3 justify-content-center">
-                    <img src={mentorIcon} width="45" height="auto"/>
+                  <div class="open-to-icons d-flex justify-content-evenly my-1">
+                    <div class="icons d-flex col p-2 mx-2 justify-content-center border border-primary rounded-circle">
+                      <img src={mentorIcon} class="" width="30" height="auto" title="Mentoring"/>
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={videoIcon} width="30" height="auto" />
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={essayIcon} width="30" height="auto"/>
+                    </div>
                   </div>
-                  <div class="d-flex col-3 justify-content-center">
-                    <img src={videoIcon} width="45" height="auto"/>
+                 
+                  <button type="button" class="btn btn-lg rounded-pill shadow-sm btn-primary my-3">Chat Now</button>
+                
+                  <div class="content-container ">
+                    <p class="pb-2 mb-2">Interests & Involvements</p>
+                    <div class="row d-flex justify-content-evenly align-items-center ">
+                      <div class="col "><p>Maker</p></div>
+                      <div class="col "><p>Full Stack Developer</p></div>
+                      <div class="col "><p>Robotics</p></div>
+                    </div>
                   </div>
-                  <div class="d-flex col-3 justify-content-center">
-                    <img src={essayIcon} width="45" height="auto"/>
-                  </div>
-                </div>
-                <div class="open-to-icons d-flex justify-content-evenly mx-4">
-                  <div class="d-flex col-3 justify-content-center">
-                    <p>Mentoring</p>
-                  </div>
-                  <div class="d-flex col-3 justify-content-center">
-                    <p>Video Call</p>
-                  </div>
-                  <div class="d-flex col-3 justify-content-center">
-                    <p>Essay Help</p>
-                  </div>
+                  
                 </div>
               </div>
             </div>
             
+            <div class="col mb-3">
+              <div className="grow container bg-white rounded-3 card shadow">
+                <div class="d-flex flex-column text-center justify-content-center align-items-center">
+                  <img src="https://picsum.photos/150/150" class="rounded-circle border p-2 my-2 mt-3 " width="150" ></img>
+                  <div class="text-container ">
+                    <p class="p-0 mb-2"><b><span class="names ">Thomas Jefferson</span></b><br />
+                    Boston College &#8226; Computer Science<br/>Brownsburg, IN</p>
+                  </div>
+                  <div class="open-to-icons d-flex justify-content-evenly my-1">
+                    <div class="icons d-flex col p-2 mx-2 justify-content-center border border-primary rounded-circle">
+                      <img src={mentorIcon} class="" width="30" height="auto" title="Mentoring"/>
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={videoIcon} width="30" height="auto" />
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={essayIcon} width="30" height="auto"/>
+                    </div>
+                  </div>
+                 
+                  <button type="button" class="btn btn-lg rounded-pill shadow-sm btn-primary my-3">Chat Now</button>
+                
+                  <div class="content-container ">
+                    <p class="pb-2 mb-2">Interests & Involvements</p>
+                    <div class="row d-flex justify-content-evenly align-items-center ">
+                      <div class="col "><p>Maker</p></div>
+                      <div class="col "><p>Full Stack Developer</p></div>
+                      <div class="col "><p>Robotics</p></div>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
 
+            <div class="col mb-3">
+              <div className="grow container bg-white rounded-3 card shadow">
+                <div class="d-flex flex-column text-center justify-content-center align-items-center">
+                  <img src="https://picsum.photos/150/150" class="rounded-circle border p-2 my-2 mt-3 " width="150" ></img>
+                  <div class="text-container ">
+                    <p class="p-0 mb-2"><b><span class="names ">Thomas Jefferson</span></b><br />
+                    Boston College &#8226; Computer Science<br/>Brownsburg, IN</p>
+                  </div>
+                  <div class="open-to-icons d-flex justify-content-evenly my-1">
+                    <div class="icons d-flex col p-2 mx-2 justify-content-center border border-primary rounded-circle">
+                      <img src={mentorIcon} class="" width="30" height="auto" title="Mentoring"/>
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={videoIcon} width="30" height="auto" />
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={essayIcon} width="30" height="auto"/>
+                    </div>
+                  </div>
+                 
+                  <button type="button" class="btn btn-lg rounded-pill shadow-sm btn-primary my-3">Chat Now</button>
+                
+                  <div class="content-container ">
+                    <p class="pb-2 mb-2">Interests & Involvements</p>
+                    <div class="row d-flex justify-content-evenly align-items-center ">
+                      <div class="col "><p>Maker</p></div>
+                      <div class="col "><p>Full Stack Developer</p></div>
+                      <div class="col "><p>Robotics</p></div>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
             
         
         </div>
         </div>
-        <div class="col-4 bg-primary">
-          <h3>Pending Connections</h3>
-          <div class="row ">
-          <div class="col bg-secondary mx-2"> 
-            <div class="d-flex justify-content-start align-items-center me-2">
-              <img src="https://picsum.photos/100/100" class="rounded-circle p-2 me-2 " width="80" ></img>
-              <div class="text-container ">
-                <p class="p-0 m-0"><b><span class="names">Thomas Jefferson</span></b><br />
-                Boston College</p>
-              </div>
-              <button type="button" class="btn btn-lg btn-primary ms-auto ">Chat</button>
-            </div>
-            <div class="content-container ">
-              <p class="p-0 m-0">Computer Science &#8226; 2025 </p>
-              <p class="p-0 m-0">From: Brownsburg, IN</p>
-              <p class="p-0 m-0">Interests & Involvements</p>
-              <ul>
-                <li>Maker</li>
-                <li>Full Stack Developer</li>
-                <li>Robotics</li>
-              </ul>
-              <p>I'm open to: </p>
-            </div>
-            <div class="open-to-icons d-flex justify-content-evenly mx-4">
-              <div class="d-flex col-3 justify-content-center">
-                <img src={mentorIcon} width="45" height="auto"/>
-              </div>
-              <div class="d-flex col-3 justify-content-center">
-                <img src={videoIcon} width="45" height="auto"/>
-              </div>
-              <div class="d-flex col-3 justify-content-center">
-                <img src={essayIcon} width="45" height="auto"/>
-              </div>
-            </div>
-            <div class="open-to-icons d-flex justify-content-evenly mx-4">
-              <div class="d-flex col-3 justify-content-center">
-                <p>Mentoring</p>
-              </div>
-              <div class="d-flex col-3 justify-content-center">
-                <p>Video Call</p>
-              </div>
-              <div class="d-flex col-3 justify-content-center">
-                <p>Essay Help</p>
+        <div class="col-3 bg-secondary rounded-3 card">
+          <i><h3 class="m-2">Pending Connections</h3></i>
+          <div class="row row-cols 1">
+            <div class="col mb-3">
+              <div className="grow container bg-white rounded-3 card shadow-sm">
+                <div class="d-flex flex-column text-center justify-content-center align-items-center">
+                  <img src="https://picsum.photos/150/150" class="rounded-circle border p-2 my-2 mt-3 " width="150" ></img>
+                  <div class="text-container ">
+                    <p class="p-0 mb-2"><b><span class="names ">Thomas Jefferson</span></b><br />
+                    <span class="last-online"><i>Last Online - Tuesday</i></span><br/>
+                    Boston College &#8226; Computer Science<br/>Brownsburg, IN</p>
+                  </div>
+                  <div class="open-to-icons d-flex justify-content-evenly my-1">
+                    <div class="icons d-flex col p-2 mx-2 justify-content-center border border-primary rounded-circle">
+                      <img src={mentorIcon} class="" width="30" height="auto" title="Mentoring"/>
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={videoIcon} width="30" height="auto" />
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={essayIcon} width="30" height="auto"/>
+                    </div>
+                  </div>
+                 
+                  <button type="button" class="btn btn-lg rounded-pill shadow-sm btn-primary my-3">Message</button>
+                
+                  <div class="content-container ">
+                    <p class="pb-2 mb-2">Interests & Involvements</p>
+                    <div class="row d-flex justify-content-evenly align-items-center ">
+                      <div class="col "><p>Maker</p></div>
+                      <div class="col "><p>Full Stack Developer</p></div>
+                      <div class="col "><p>Robotics</p></div>
+                    </div>
+                  </div>
+                  </div> 
+                </div>
               </div>
             </div>
+
+            <div class="col mb-3">
+              <div className="grow container bg-white rounded-3 card shadow-sm">
+                <div class="d-flex flex-column text-center justify-content-center align-items-center">
+                  <img src="https://picsum.photos/150/150" class="rounded-circle border p-2 my-2 mt-3 " width="150" ></img>
+                  <div class="text-container ">
+                    <p class="p-0 mb-2"><b><span class="names ">Thomas Jefferson</span></b><br />
+                    <span class="last-online"><i>Last Online - Tuesday</i></span><br/>
+                    Boston College &#8226; Computer Science<br/>Brownsburg, IN</p>
+                  </div>
+                  <div class="open-to-icons d-flex justify-content-evenly my-1">
+                    <div class="icons d-flex col p-2 mx-2 justify-content-center border border-primary rounded-circle">
+                      <img src={mentorIcon} class="" width="30" height="auto" title="Mentoring"/>
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={videoIcon} width="30" height="auto" />
+                    </div>
+                    <div class="d-flex col mx-2 p-2 justify-content-center border border-primary rounded-circle">
+                      <img src={essayIcon} width="30" height="auto"/>
+                    </div>
+                  </div>
+                 
+                  <button type="button" class="btn btn-lg rounded-pill shadow-sm btn-primary my-3">Message</button>
+                
+                  <div class="content-container ">
+                    <p class="pb-2 mb-2">Interests & Involvements</p>
+                    <div class="row d-flex justify-content-evenly align-items-center ">
+                      <div class="col "><p>Maker</p></div>
+                      <div class="col "><p>Full Stack Developer</p></div>
+                      <div class="col "><p>Robotics</p></div>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+
             </div>
             </div>
-        </div>
-      </div>
       
     </>
   );
